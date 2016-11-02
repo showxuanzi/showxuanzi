48ac1189e2c37ef545d4be8400ea423875d1c4e4
showxuanzi <15369302901@163.com>
2016-11-02 23:11:00 +0800

    showxuanzi

---
+++ js/detail.js
@@ -0,0 +1,61 @@
+/**
+ * Created by hasee on 2016/7/14.
+ */
+require.config({
+    paths:{
+        "zeptoQuery":"lib/zepto.min",
+        "iscroll":"lib/iscroll",
+        "swiper":"lib/swiper.jquery",
+        "fnBase":"lib/fnBase"
+    },
+    shim:{
+        "iscroll":{
+            exports:"IScroll"
+        },
+        "zeptoQuery":{
+            exports:"Zepto"
+        },
+        "swiper":{
+            exports:"Swiper"
+        }
+    }
+});
+require(["zeptoQuery"],function($){
+    require(["swiper","fnBase"],function(Swiper,fnBase){
+        fnBase.myalert()
+        var goodsID = fnBase.request("goodsID");
+        window.$ =window.Zepto = $;
+
+        $.get("http://datainfo.duapp.com/shopdata/getGoods.php?callback=?",{"goodsID":goodsID},function(data){
+            var aImg = JSON.parse(data[0].imgsUrl);
+
+            var str = "";
+            for(var i=0; i<aImg.length;i++){
+                str+='<div class="swiper-slide"><img src="'+aImg[i]+'" /></div>'
+            }
+
+
+            $("#detail-page .swiper-wrapper").html(str);
+            var mySwiper = new Swiper('.swiper-container', {
+                pagination: '.self-pagination',
+                slidesPerView: '3',
+                loop:true
+            });
+        });
+
+        $("#detail-page .add-cart").on("tap",function(){
+            $.get("http://datainfo.duapp.com/shopdata/updatecar.php"
+                ,{userID:fnBase.getUserId(),goodsID:goodsID,number:1},
+                function(data){
+                    if(data==1){
+                        alert("添加成功")
+                    }
+
+                })
+        })
+
+
+    });
+});
+
+
